b1271a8a63c2ffc2dcbbd9b60b620bcd7a7ebb47
luisarg03 <luis.m.paz.03@gmail.com>
2024-08-03 19:32:08 -0300

    chore: Update Streamlit app configuration and UI

--- app/app.py
+++ app/app.py
@@ -1,23 +1,25 @@
+from openai import OpenAI
 import streamlit as st
-from langchain_openai.chat_models import ChatOpenAI
 
-st.title("🦜🔗 Quickstart App")
-
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+with st.sidebar:
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://github.com/Luisarg03/StreamlitLangchainBot)"
 
 
-def generate_response(input_text):
-    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
-    st.info(model.invoke(input_text))
+st.image("./img/logo_cat.png", width=200)
+st.title("💬 SAMI") 
+st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"], avatar='🐱').write(msg["content"])
 
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
-    )
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key.startswith("sk-"):
-        st.warning("Please enter your OpenAI API key!", icon="⚠")
-    if submitted and openai_api_key.startswith("sk-"):
-        generate_response(text)
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant", avatar='🐱').write(msg)
